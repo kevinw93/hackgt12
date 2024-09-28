3af9c31469ab0e3c4758e1d96bb899b097d98357
kevinw93 <kevinwangatl@gmail.com>
2024-09-28 10:57:31 -0400

    sussy:

---
+++ pages/dashboard.js
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold">Welcome to your Dashboard</h1>
+      </div>
+    );
+  }
